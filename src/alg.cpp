280d624e01464c0b1ca319d10e96f15ca64ba2c4
Казарина Катя <94719822+KazarinaKate@users.noreply.github.com>
2022-02-25 04:50:16 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,14 +7,14 @@ bool checkPrime(uint64_t value) {
 // вставьте код функции
     int z = value;
     if (z > 1) {
-	    for (int i = 2; i < z; i++) {
-		    if (z % i == 0) {
-			    return false;
-		    }
-		    return true;
-	    } 
+        for (int i = 2; i < z; i++) {
+            if (z % i == 0) {
+                return false;
+            }
+            return true;
+        }
     } else {
-	    return false;
+        return false;
     }
 }
 
@@ -23,15 +23,15 @@ uint64_t nPrime(uint64_t n) {
     int p = 0;
     int N = n;
     uint64_t i = 2;
-    for (i; ; i++){
-	    if (checkPrime(i)) {
-		    p++;
-	    }
-	    if (p == N) {
-		    break;
-	    }
+    for (i; ; i++) {
+        if (checkPrime(i)) {
+            p++;
+        }
+        if (p == N) {
+            break;
+        }
     }
-	return i;
+    return i;
 }
 
 uint64_t nextPrime(uint64_t value) {
@@ -39,11 +39,11 @@ uint64_t nextPrime(uint64_t value) {
     int z = value;
     uint64_t i = 2;
     for (i; ; i++) {
-	    if (checkPrime(i)) {
-		    if (i > z) {
-			    return i;
-		    }
-	    }
+        if (checkPrime(i)) {
+            if (i > z) {
+                return i;
+            }
+        }
     }
 }
 
@@ -53,9 +53,9 @@ uint64_t sumPrime(uint64_t hbound) {
     uint64_t sum = 0;
     int i = 2;
     for (i; i < z; i++) {
-	    if (checkPrime(i)) {
-		    sum = sum + i;
-	    }
+        if (checkPrime(i)) {
+            sum = sum + i;
+        }
     }
-	return sum;
+    return sum;
 }
