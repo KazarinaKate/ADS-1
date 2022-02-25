a9e32a6702369bc94d3b699f51c3e3c3a4019952
Казарина Катя <94719822+KazarinaKate@users.noreply.github.com>
2022-02-25 04:24:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,56 +1,61 @@
 // Copyright 2022 NNTU-CS
+#include <cstdint>
+#include "alg.h"
+
+
 bool checkPrime(uint64_t value) {
 // вставьте код функции
     int z = value;
     if (z > 1) {
-		for (int i = 2; i < z; i++)
-			if (z % i == 0) {
-				return false;
-			}
-			return true;
-	} else {
-		return false;
-	}
+	    for (int i = 2; i < z; i++) {
+		    if (z % i == 0) {
+			    return false;
+		    }
+		    return true;
+	    } 
+    } else {
+	    return false;
+    }
 }
 
 uint64_t nPrime(uint64_t n) {
 // вставьте код функции
     int p = 0;
-	int N = n;
-	int i = 2;
-	for (i; ; i++){
-		if (checkPrime(i)) {
-			p++;
-		}
-		if (p == N) {
-			break;
-		}
-	}
+    int N = n;
+    uint64_t i = 2;
+    for (i; ; i++){
+	    if (checkPrime(i)) {
+		    p++;
+	    }
+	    if (p == N) {
+		    break;
+	    }
+    }
 	return i;
 }
 
 uint64_t nextPrime(uint64_t value) {
 // вставьте код функции
     int z = value;
-	int i = 2;
-	for (i; ; i++) {
-		if (checkPrime(i)) {
-			if (i > z) {
-				return i;
-			}
-		}
-	}
+    uint64_t i = 2;
+    for (i; ; i++) {
+	    if (checkPrime(i)) {
+		    if (i > z) {
+			    return i;
+		    }
+	    }
+    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
 // вставьте код функции
     int z = hbound;
-	int sum = 0;
-	int i = 2;
-	for (i; i < z; i++) {
-		if (checkPrime(i)) {
-			sum = sum + i;
-		}
-	}
+    uint64_t sum = 0;
+    int i = 2;
+    for (i; i < z; i++) {
+	    if (checkPrime(i)) {
+		    sum = sum + i;
+	    }
+    }
 	return sum;
 }
