a122d21a9af8599150b6ed65f448d355de8dc478
Казарина Катя <94719822+KazarinaKate@users.noreply.github.com>
2022-02-25 03:50:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,83 +1,55 @@
 // Copyright 2022 NNTU-CS
 bool checkPrime(uint64_t value) {
 // вставьте код функции
-    if (value > 1) {
-        for (int i = 2; i < value; i++) {
-			if (value % i == 0) {
+    int z = value;
+	if (z > 1) {
+		for (int i = 2; i < z; i++)
+			if (z % i == 0) {
 				return false;
 			}
 			return true;
-        } else {
-            return false;
-        }
-    }
+	} else {
+		return false;
+	}
 }
 
 uint64_t nPrime(uint64_t n) {
 // вставьте код функции
     int p = 0;
-	bool a = true;
-	for (int i = 2; i < 10000; i++) {
-	    a = true;
-	    for (int j = 2; j <= (i / 2); j++) {
-	        if (i != j) {
-	            if (i % j == 0) {
-	                a = false;
-	            }
-	            if (!a) {
-	                break;
-	            }
-	        }
-	    }
-	    if (a) {
-	        p++;
-	    }
-	    if (p == n) {
-	        return i;
-	        break;
-	    }
+	int N = n;
+	int i = 2;
+	for (i; ; i++){
+		if (checkPrime(i)) {
+			p++;
+		}
+		if (p == N) {
+			break;
+		}
 	}
+	return i;
 }
 
 uint64_t nextPrime(uint64_t value) {
 // вставьте код функции
-    bool a = true;
-    for (int i = value + 1; i < 10000; i++) {
-        a = true;
-        for (int j = 2; j <= (i / 2); ++j) {
-            if (i != j) {
-                if (i % j == 0) {
-                    a = false;
-                }
-                if (!a) {
-                    break;
-				}
+    int z = value;
+	int i = 2;
+	for (i; ; i++) {
+		if (checkPrime(i)) {
+			if (i > z) {
+				return i;
 			}
-		}
-		if (a) {
-		    return i;
 		}
 	}
 }
 
 uint64_t sumPrime(uint64_t hbound) {
 // вставьте код функции
-    int sum = 0;
-    bool a = true;
-    for (int i = 2; i < hbound; i++) {
-        a = true;
-        for (int j = 2; j <= (i / 2); ++j) {
-            if (i != j) {
-                if (i % j == 0) {
-                    a = false;
-				}
-				if (!a) {
-				    break;
-				}
-			}
-		}
-		if (a) {
-		    sum = sum + i;
+    int z = hbound;
+	int sum = 0;
+	int i = 2;
+	for (i; i < z; i++) {
+		if (checkPrime(i)) {
+			sum = sum + i;
 		}
 	}
 	return sum;
